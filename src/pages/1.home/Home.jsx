807e3c66bc8df700b0c871c6826eabd4b8b975db
Andres Garza <hanato21@icloud.com>
2023-12-12 17:59:06 -0600

    Update

--- src/pages/1.home/Home.jsx
+++ src/pages/1.home/Home.jsx
@@ -1,10 +1,18 @@
 import truckImg from "../../assets/800318861.jpg";
+import stiLogo from "../../assets/sti-logo-black-yellow.png";
 
 const Home = () => {
   return (
     <div className="bg-slate-950">
-      <img src={truckImg} className="w-full" />
-      <main></main>
+      <main>
+        <img src={truckImg} className="w-full" />
+        <div className="static rounded-md md:relative md:max-w-[80%] mx-auto md:top-[-10rem] xl:top-[-22rem] shadow-md bg-slate-950">
+          <h1 className=" text-yellow-300 font-bold text-4xl md:text-6xl xl:text-9xl text-center py-7 font-systemui">
+            Solo Transport Inc.
+          </h1>
+          <img src={stiLogo} className="py-4 mx-auto" />
+        </div>
+      </main>
     </div>
   );
 };
