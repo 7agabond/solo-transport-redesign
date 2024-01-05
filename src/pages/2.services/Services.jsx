b243901021d7cf5fa20c6fc567feb4cf937733c6
Andres Garza <hanato21@icloud.com>
2024-01-05 17:25:26 -0600

    Services completed

--- src/pages/2.services/Services.jsx
+++ src/pages/2.services/Services.jsx
@@ -67,14 +67,14 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   We'll Deliver{" "}
                   <span className="text-yellow-400">Internationally.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   With a network of carriers and terminals across North America,
@@ -91,14 +91,14 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   FTL & LTL
                   <span className="text-yellow-400">.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   We'll move your Full Truckload or Less Than Truckload freight
@@ -114,13 +114,13 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   Infrastructure<span className="text-yellow-400">.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   Our secure yard is located near the World Trade International
@@ -138,14 +138,14 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   Specialized Freight
                   <span className="text-yellow-400">.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   We have very competent drivers, partners and dedicated
@@ -162,14 +162,14 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   Industry Standard{" "}
                   <span className="text-yellow-400">Technology.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   We operate our fleet of tractors with industry standard GPS
@@ -186,13 +186,13 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.6rem] mb-[0.5rem] leading-none`}
                 >
                   Customer <span className="text-yellow-400">Support.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.2rem]`}
                 >
                   We strive for honesty and transparency with you and everybody
@@ -208,7 +208,7 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-[white]" : ""
-                  } font-bold tracking-wide text-[1.6rem] mt-[1rem] mb-[0.5rem]`}
+                  } font-bold tracking-wide text-[1.8rem] mt-[1rem] mb-[0.5rem] leading-none`}
                 >
                   We offer a variety of services
                   <span className="text-yellow-400">.</span>
@@ -216,7 +216,7 @@ const Services = (props) => {
                 <ul
                   className={`${
                     props.darkMode ? "text-gray-400" : "text-gray-800"
-                  } text-[1.1rem] list-none`}
+                  } text-[1.1rem] list-none font-light`}
                 >
                   <li className="mb-[0.3rem]">Bonded Freight to/from Canada</li>
                   <li className="mb-[0.3rem]">
@@ -241,14 +241,14 @@ const Services = (props) => {
                 <h1
                   className={`${
                     props.darkMode ? "text-white" : ""
-                  } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                  } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                 >
                   We'll Deliver{" "}
                   <span className="text-yellow-400">Internationally.</span>
                 </h1>
                 <p
                   className={`${
-                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                    props.darkMode ? "text-gray-400" : "text-gray-500"
                   } text-[1.7rem]`}
                 >
                   With a network of carriers and terminals across North America,
@@ -256,7 +256,7 @@ const Services = (props) => {
                   Canada and Mexico, as well as the Contiguous USA.
                 </p>
               </div>
-              <div className="services-grid p-[2rem] w-[70%] m-auto">
+              <div className="services-grid p-[2rem] w-[75%] m-auto">
                 <div
                   data-aos="fade-right"
                   data-aos-anchor-placement="bottom-bottom"
@@ -266,13 +266,13 @@ const Services = (props) => {
                   <h1
                     className={`${
                       props.darkMode ? "text-white" : ""
-                    } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                    } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                   >
                     FTL & LTL<span className="text-yellow-400">.</span>
                   </h1>
                   <p
                     className={`${
-                      props.darkMode ? "text-gray-400" : "text-gray-800"
+                      props.darkMode ? "text-gray-400" : "text-gray-500"
                     } text-[1.7rem]`}
                   >
                     We'll move your Full Truckload or Less Than Truckload
@@ -288,13 +288,13 @@ const Services = (props) => {
                   <h1
                     className={`${
                       props.darkMode ? "text-white" : ""
-                    } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                    } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                   >
                     Infrastructure<span className="text-yellow-400">.</span>
                   </h1>
                   <p
                     className={`${
-                      props.darkMode ? "text-gray-400" : "text-gray-800"
+                      props.darkMode ? "text-gray-400" : "text-gray-500"
                     } text-[1.7rem]`}
                   >
                     Our secure yard is located near the World Trade
@@ -312,14 +312,14 @@ const Services = (props) => {
                   <h1
                     className={`${
                       props.darkMode ? "text-white" : ""
-                    } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                    } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                   >
                     Specialized Freight
                     <span className="text-yellow-400">.</span>
                   </h1>
                   <p
                     className={`${
-                      props.darkMode ? "text-gray-400" : "text-gray-800"
+                      props.darkMode ? "text-gray-400" : "text-gray-500"
                     } text-[1.7rem]`}
                   >
                     We have very competent drivers, partners and dedicated
@@ -338,14 +338,14 @@ const Services = (props) => {
                   <h1
                     className={`${
                       props.darkMode ? "text-white" : ""
-                    } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                    } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                   >
                     Industry Standard{" "}
                     <span className="text-yellow-400">Technology.</span>
                   </h1>
                   <p
                     className={`${
-                      props.darkMode ? "text-gray-400" : "text-gray-800"
+                      props.darkMode ? "text-gray-400" : "text-gray-500"
                     } text-[1.7rem]`}
                   >
                     We operate our fleet of tractors with industry standard GPS
@@ -363,19 +363,49 @@ const Services = (props) => {
                   <h1
                     className={`${
                       props.darkMode ? "text-white" : ""
-                    } font-bold tracking-wide text-[2rem] mb-[1rem]`}
+                    } font-bold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
                   >
                     Customer <span className="text-yellow-400">Support.</span>
                   </h1>
                   <p
                     className={`${
-                      props.darkMode ? "text-gray-400" : "text-gray-800"
+                      props.darkMode ? "text-gray-400" : "text-gray-500"
                     } text-[1.7rem]`}
                   >
                     We strive for honesty and transparency with you and
                     everybody we work with.
                   </p>
                 </div>
+              </div>
+              <div
+                data-aos="fade"
+                data-aos-once="false"
+                data-aos-anchor-placement="bottom-bottom"
+                className="text-center p-[1.7rem]"
+              >
+                <h1
+                  className={`${
+                    props.darkMode ? "text-white" : ""
+                  } font-bold tracking-wide text-[3rem] mb-[1rem]`}
+                >
+                  We offer a variety of services
+                  <span className="text-yellow-400">.</span>
+                </h1>
+                <ul
+                  className={`${
+                    props.darkMode ? "text-gray-400" : "text-gray-800"
+                  } text-[1.7rem] list-none font-light`}
+                >
+                  <li className="mb-[0.3rem]">Bonded Freight to/from Canada</li>
+                  <li className="mb-[0.3rem]">
+                    Door to Door Service US/Mexico
+                  </li>
+                  <li className="mb-[0.3rem]">Mexican Transportation</li>
+                  <li className="mb-[0.3rem]">Border Crossing</li>
+                  <li className="mb-[0.3rem]">Transloading</li>
+                  <li className="mb-[0.3rem]">Drayage</li>
+                  <li className="mb-[0.3rem]">Storage</li>
+                </ul>
               </div>
             </>
           )}
