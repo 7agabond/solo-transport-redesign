d3d3ce45209a5f37c69b9a0b7d23fdf9ff1a9630
Andres Garza <hanato21@icloud.com>
2024-01-08 17:12:00 -0600

    Working on About page layout

--- src/pages/3.about/About.jsx
+++ src/pages/3.about/About.jsx
@@ -16,7 +16,21 @@ const About = (props) => {
 
   return (
     <>
-      <div>About</div>
+      <div>
+        <div
+          className={`${
+            props.darkMode ? "bg-gray-300" : "bg-gray-900"
+          } relative w-full h-[30vh]`}
+        >
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-900" : "text-white"
+            } absolute text-[2rem] md:text-[4rem] font-bold top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] container-fadein`}
+          >
+            About <span className="text-yellow-300">Solo.</span>
+          </h1>
+        </div>
+      </div>
       <div className="loader-wrapper">
         <span className="loader">
           <span className="loader-inner"></span>
