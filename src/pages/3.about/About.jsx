bc7711f885142b474168adcd6737c9fd6b3c2b7a
Andres Garza <hanato21@icloud.com>
2024-01-08 13:17:47 -0600

    Added loader and background animator

--- src/pages/3.about/About.jsx
+++ src/pages/3.about/About.jsx
@@ -1,5 +1,29 @@
-const About = () => {
-  return <div>About</div>;
+import { useEffect } from "react";
+import { animate } from "motion";
+import $ from "jquery";
+
+const About = (props) => {
+  useEffect(() => {
+    $(window).on("load", () => {
+      $(".loader-wrapper").fadeOut("slow");
+    });
+  }, []);
+
+  props.darkMode
+    ? animate("body", { backgroundColor: "rgb(2 6 23)" })
+    : props.darkMode === false &&
+      animate("body", { backgroundColor: "initial" });
+
+  return (
+    <>
+      <div>About</div>
+      <div className="loader-wrapper">
+        <span className="loader">
+          <span className="loader-inner"></span>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/pages/4.contact/Contact.jsx
+++ src/pages/4.contact/Contact.jsx
@@ -1,5 +1,29 @@
-const Contact = () => {
-  return <div>Contact</div>;
+import { useEffect } from "react";
+import { animate } from "motion";
+import $ from "jquery";
+
+const Contact = (props) => {
+  useEffect(() => {
+    $(window).on("load", () => {
+      $(".loader-wrapper").fadeOut("slow");
+    });
+  }, []);
+
+  props.darkMode
+    ? animate("body", { backgroundColor: "rgb(2 6 23)" })
+    : props.darkMode === false &&
+      animate("body", { backgroundColor: "initial" });
+
+  return (
+    <>
+      <div>Contact</div>
+      <div className="loader-wrapper">
+        <span className="loader">
+          <span className="loader-inner"></span>
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
