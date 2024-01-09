26e5d5482ec99641c4a1630e6219b2509b8c385b
Andres Garza <hanato21@icloud.com>
2024-01-09 12:06:49 -0600

    Latest update

--- src/pages/3.about/About.jsx
+++ src/pages/3.about/About.jsx
@@ -1,10 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { animate } from "motion";
 import forkliftImg from "../../assets/forklift.jpg";
 import platformImg from "../../assets/img-2584_orig.jpg";
 import $ from "jquery";
 
 const About = (props) => {
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth < 768 ? true : false
+  );
+
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+      console.log(isMobile);
+    } else {
+      setIsMobile(false);
+      console.log(isMobile);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   useEffect(() => {
     $(window).on("load", () => {
       $(".loader-wrapper").fadeOut("slow");
@@ -18,24 +36,34 @@ const About = (props) => {
 
   return (
     <>
-      <div>
-        <div
+      <div
+        className={`${
+          props.darkMode ? "bg-gray-300" : "bg-gray-900"
+        } relative w-full h-[30vh]`}
+      >
+        <h1
           className={`${
-            props.darkMode ? "bg-gray-300" : "bg-gray-900"
-          } relative w-full h-[30vh]`}
+            props.darkMode ? "text-gray-900" : "text-white"
+          } absolute text-[2rem] md:text-[4rem] font-semibold top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] container-fadein md:tracking-wider`}
         >
-          <h1
-            className={`${
-              props.darkMode ? "text-gray-900" : "text-white"
-            } absolute text-[2rem] md:text-[4rem] font-semibold top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] container-fadein md:tracking-wider`}
-          >
-            About <span className="text-yellow-300">Solo.</span>
-          </h1>
-        </div>
-        <div className="services-grid-2 p-[2rem] max-w-[1200px] m-auto">
-          <div>
-            <h1>Established in 1998.</h1>
-            <p>
+          About <span className="text-yellow-300">Solo.</span>
+        </h1>
+      </div>
+      {isMobile ? (
+        <div>
+          <div className="text-center">
+            <h1
+              className={`${
+                props.darkMode ? "text-white" : ""
+              } pt-[2rem] font-semibold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
+            >
+              Established in <span className="text-yellow-300">1998.</span>
+            </h1>
+            <p
+              className={`${
+                props.darkMode ? "text-gray-400" : "text-gray-500"
+              } px-[1.5rem] text-[1.3rem]`}
+            >
               In August 1998, Solo started out as a modest brokerage with a few
               customers trying to differentiate itself from the rest by being
               transparent. We've grown into a capable provider, offering a wide
@@ -43,19 +71,93 @@ const About = (props) => {
               to stay competitive in this industry. Laredo is our home but we
               have partners all across the continent.
             </p>
+            <img src={forkliftImg} className="py-[2rem]" />
           </div>
-          <div>
-            <h1>Our Goal.</h1>
-            <p>
+          <div className="text-center">
+            <h1
+              className={`${
+                props.darkMode ? "text-white" : ""
+              } px-[1rem] font-semibold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
+            >
+              Our <span className="text-yellow-300">Goal.</span>
+            </h1>
+            <p
+              className={`${
+                props.darkMode ? "text-gray-400" : "text-gray-500"
+              } px-[1.5rem] text-[1.3rem]`}
+            >
               To deliver your products safely and on time. We want to develop
               lasting relationships with our Customers and Carriers.
             </p>
+            <img src={platformImg} className="py-[2rem]" />
           </div>
-          <img src={forkliftImg} className="h-[500px] object-cover" />
-          <img src={platformImg} className="h-[500px] object-cover" />
+          <h1
+            className={`${
+              props.darkMode ? "text-white" : ""
+            } px-[1rem] text-center font-bold tracking-wide text-[2.3rem] pt-[4rem] pb-[9rem]`}
+          >
+            We're Here to <span className="text-yellow-300">Help.</span>
+          </h1>
         </div>
-        <h1>We're Here to Help.</h1>
-      </div>
+      ) : (
+        <div>
+          <div className="services-grid-2 p-[2rem] max-w-[1200px] m-auto">
+            <div className="text-center">
+              <h1
+                className={`${
+                  props.darkMode ? "text-white" : ""
+                } font-semibold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
+              >
+                Established in <span className="text-yellow-300">1998.</span>
+              </h1>
+              <p
+                className={`${
+                  props.darkMode ? "text-gray-400" : "text-gray-500"
+                } text-[1.5rem]`}
+              >
+                In August 1998, Solo started out as a modest brokerage with a
+                few customers trying to differentiate itself from the rest by
+                being transparent. We've grown into a capable provider, offering
+                a wide range of services. We have decades of experience and have
+                managed to stay competitive in this industry. Laredo is our home
+                but we have partners all across the continent.
+              </p>
+            </div>
+            <div className="text-center">
+              <h1
+                className={`${
+                  props.darkMode ? "text-white" : ""
+                } font-semibold tracking-wide text-[2rem] mb-[1rem] leading-tight`}
+              >
+                Our <span className="text-yellow-300">Goal.</span>
+              </h1>
+              <p
+                className={`${
+                  props.darkMode ? "text-gray-400" : "text-gray-500"
+                } text-[1.5rem]`}
+              >
+                To deliver your products safely and on time. We want to develop
+                lasting relationships with our Customers and Carriers.
+              </p>
+            </div>
+            <img
+              src={forkliftImg}
+              className="rounded-md h-[500px] object-cover"
+            />
+            <img
+              src={platformImg}
+              className="rounded-md h-[500px] object-cover"
+            />
+          </div>
+          <h1
+            className={`${
+              props.darkMode ? "text-white" : ""
+            } text-center font-bold tracking-wide text-[2.5rem] pt-[5rem] pb-[7rem]`}
+          >
+            We're Here to <span className="text-yellow-300">Help.</span>
+          </h1>
+        </div>
+      )}
       <div className="loader-wrapper">
         <span className="loader">
           <span className="loader-inner"></span>
