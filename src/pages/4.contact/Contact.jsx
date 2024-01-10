ca6784db5c88d6b1a3b6ae7a5101dbf6f78a18f6
Andres Garza <hanato21@icloud.com>
2024-01-10 15:14:18 -0600

    First release

--- src/pages/4.contact/Contact.jsx
+++ src/pages/4.contact/Contact.jsx
@@ -1,5 +1,7 @@
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { useEffect } from "react";
 import { animate } from "motion";
+import "leaflet/dist/leaflet.css";
 import $ from "jquery";
 
 const Contact = (props) => {
@@ -28,6 +30,93 @@ const Contact = (props) => {
         >
           Contact Us<span className="text-yellow-300">.</span>
         </h1>
+      </div>
+      <div className="contact-grid">
+        <div className="container-fadein fadein-left">
+          <MapContainer center={[27.6137701, -99.536271]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[27.6137701, -99.536271]}></Marker>
+          </MapContainer>
+        </div>
+        <div className="fadein-right m-auto">
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-300" : "text-gray-800"
+            } pt-[2rem] pb-[1rem] px-[2rem] max-w-[800px] text-[1.2rem] font-semibold text-center`}
+          >
+            Let us know how we can help you meet your needs. Contact us via
+            email to get started with a rate quote or give us a call so that we
+            may address your concerns.
+          </h1>
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-300" : "text-gray-800"
+            } p-[1rem] text-[1.2rem] font-semibold text-center`}
+          >
+            We're ready for your business.
+          </h1>
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-300" : "text-gray-800"
+            } p-[1rem] text-[1.2rem] font-semibold text-center`}
+          >
+            email:
+          </h1>
+          <p
+            className={`${
+              props.darkMode ? "text-yellow-300" : "text-yellow-500"
+            } p-[1rem] text-[1.2rem] font-light text-center`}
+          >
+            <a
+              href="mailto:quote@solotransport.com"
+              className="hover:underline"
+            >
+              quote@solotransport.com
+            </a>
+          </p>
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-300" : "text-gray-800"
+            } p-[1rem] text-[1.2rem] font-semibold text-center`}
+          >
+            phone:
+          </h1>
+          <p
+            className={`${
+              props.darkMode ? "text-yellow-300" : "text-yellow-500"
+            } p-[1rem] text-[1.2rem] font-light text-center`}
+          >
+            (956) 712-4244
+          </p>
+          <h1
+            className={`${
+              props.darkMode ? "text-gray-300" : "text-gray-800"
+            } p-[1rem] text-[1.2rem] font-semibold text-center`}
+          >
+            address:
+          </h1>
+          <p
+            className={`${
+              props.darkMode ? "text-yellow-300" : "text-yellow-500"
+            } text-[1.2rem] font-light text-center`}
+          >
+            628 Enterprise St.
+          </p>
+          <p
+            className={`${
+              props.darkMode ? "text-yellow-300" : "text-yellow-500"
+            } text-[1.2rem] font-light text-center`}
+          >
+            Laredo, Texas 78045
+          </p>
+          <p
+            className={`${
+              props.darkMode ? "text-yellow-300" : "text-yellow-500"
+            } text-[1.2rem] font-light text-center`}
+          >
+            Interamerica Distribution Park
+          </p>
+        </div>
       </div>
       <div className="loader-wrapper">
         <span className="loader">
